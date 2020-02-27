f321df8eda8198116d8026d0275b00879aab97c7
kyouma589 <rlothian589@gmail.com>
2020-02-27 10:51:13 -0500

    Added Raycasting to JubeiPH

---
+++ Assets/Scripts/JubeiRaycast.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JubeiRaycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float RaycastDistance = 3f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray JubeiForward = new Ray(transform.position, transform.forward);
+
+        Debug.DrawRay(JubeiForward.origin, JubeiForward.direction * RaycastDistance, Color.cyan);
+
+        if (Physics.Raycast(JubeiForward, RaycastDistance))
+        {
+
+            Debug.Log("Meow");
+
+
+
+
+        }
+
+        else
+        {
+            Debug.Log("Purrrrr");
+        }
+
+    }
+}
