e29830c0afc1e42b5499a2031642f2744dd53b01
kyouma589 <rlothian589@gmail.com>
2020-03-06 14:35:16 -0500

    Food now spawns when approaching bowl Textures added to buildings

---
+++ Assets/Scripts/FoodInstantiate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodInstantiate : MonoBehaviour
+{
+    // <summary>
+    // Start with bowl empty
+    // Bowl gets fill
+    // Eat and empty bowl
+    
+    public GameObject foodBowlFill;
+    public GameObject foodBowlEmpty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foodBowlEmpty.SetActive(true);
+        foodBowlFill.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foodBowlFill.SetActive(true);
+        foodBowlEmpty.SetActive(false);
+        Debug.Log("food bowl is now full " + other.name);
+        GetComponent<BoxCollider>().enabled = false;
+    }
+}
