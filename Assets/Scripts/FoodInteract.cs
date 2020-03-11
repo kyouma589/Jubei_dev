2f29417caa606467ecbcd317454226a08a40b67c
kyouma589 <rlothian589@gmail.com>
2020-03-11 10:59:05 -0400

    updated ui text to show when near food Added brick texture and glass to windows

---
+++ Assets/Scripts/FoodInteract.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodInteract : MonoBehaviour
+{
+    public GameObject destinationFood;
+    public float distanceToFood;
+    public Text Dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distanceToFood = Vector3.Distance(this.transform.position, destinationFood.transform.position);
+        Debug.Log(distanceToFood);
+
+        if (distanceToFood <= 2)
+        {
+            Debug.Log("hello");
+            Dialogue.text = "Press 'E' to eat food.";
+        }
+
+    }
+}
