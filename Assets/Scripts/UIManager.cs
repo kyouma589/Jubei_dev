2472425bd33c2ebb7435e3a886f24a122dc27375
kyouma589 <rlothian589@gmail.com>
2020-03-26 11:09:21 -0400

    fixed wall boundaries fixed ending script

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,27 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    FoodInteract FoodInteract;
-    DrainDetect DrainDetect;
-    MeowFunction MeowFunction;
+
+    public FoodInteract FoodInteract;
+    public DrainDetect DrainDetect;
+    public MeowFunction MeowFunction;
+    public MeowFunction MeowFunction2;
+    public MeowFunction MeowFunction3;
     public bool reqsMet = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        FoodInteract = GetComponent<FoodInteract>();
-        DrainDetect = GetComponent<DrainDetect>();
-        MeowFunction = GetComponent<MeowFunction>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (FoodInteract.eatenCounter ==1 && DrainDetect.sniffCounter <= 1 && MeowFunction.meowCounter <= 1)
+        if (FoodInteract.eatenCounter ==1 && DrainDetect.sniffCounter >= 1 && (MeowFunction.meowCounter >= 1 || MeowFunction2.meowCounter >=1 || MeowFunction3.meowCounter >= 1))
         {
             Debug.Log("Game ends here");
             reqsMet = true;
